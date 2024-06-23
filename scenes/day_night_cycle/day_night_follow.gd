8b851d95d0915020917abcfd4fb7a176e865c8f4
sosafacun <facundososa+github@slmail.me>
2024-06-23 12:06:48 -0300

    Finished the Day / Night cycle
    But at what cost

--- scenes/day_night_cycle/day_night_follow.gd
+++ scenes/day_night_cycle/day_night_follow.gd
@@ -1,23 +1,32 @@
 extends PathFollow2D
 
+@export var is_sun: bool
+
 signal daylight()
 signal nightlight()
 
 const SET_RATIO: float = 0.00005
 const UP_RATIO: float = 0.0001
 
+func _ready():
+	if(is_sun):
+		$Sun.visible = true
+		$Moon.visible = false
+	else:
+		$Sun.visible = false
+		$Moon.visible = true
+
 func _process(_delta):
+	
 	if(Globals.is_game_active):
 		progress_ratio += UP_RATIO
 	
-	if(progress_ratio < 0.7117): 
+	if(progress_ratio < 0.4976): 
 		$Time.color = Color("e19c00")
-		$Sun.visible = true
 		daylight.emit()
 		
-	if(progress_ratio >= 0.7117):
+	if(progress_ratio >= 0.4976):
 		$Time.color = Color ("006fcb")
-		$Sun.visible = false
 		nightlight.emit()
 	
 	
--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,4 +1,7 @@
 extends Control
 
+func _ready():
+	pass
+
 func _on_start_game_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")

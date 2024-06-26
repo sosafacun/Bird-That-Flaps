fb24d66b1ed48c917820156289aa55b97237fde9
sosafacun <facundososa+github@slmail.me>
2024-06-26 13:54:39 -0300

    Only the Parallax is missing.

--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -2,8 +2,18 @@ extends Control
 
 func _ready():
 	SFX.play_bgm()
-	$Label2.text = Globals.save_data.top_player
-	$Label.text = str(Globals.save_data.top_score)
+	update_labels()
 
 func _on_start_game_pressed():
 	get_tree().change_scene_to_file("res://scenes/Parallax/game.tscn")
+
+func _on_restart_scores_pressed():
+	Globals.save_data.top_score = 0
+	Globals.save_data.top_player = ""
+	$Label4.visible = true
+	Globals.save_data.save_scores()
+	update_labels()
+
+func update_labels():
+	$Label2.text = Globals.save_data.top_player
+	$Label.text = str(Globals.save_data.top_score)
